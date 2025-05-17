3d227e017633eece1dd6447eec0469e065376df1
Prasil <prasil.suthar@gmail.com>
2025-05-17 15:55:19 +0530

    Take Input

--- main.java
+++ main.java
@@ -3,10 +3,17 @@ import java.util.*;
 public class main{
 public static void main(String[] args) {
     //VARIEABLES
-    int a = 10;
-    int b = 20;
-    int sum = a+b;
-    
-    System.out.println(sum);
+    // int a = 10;
+    // int b = 20;
+    // int sum = a+b;
+
+    // System.out.println(sum);
+
+    //Take Input
+    Scanner sc  = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println(name);
+
+
     }
 }
